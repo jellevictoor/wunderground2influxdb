4f2bdfe757e2a1dc1e5046ff43164fd16606d950
Jelle Victoor <jelle@victoor.io>
2024-08-12 19:34:39 +0200

    cleanup the script with typer

--- src/main.py
+++ src/main.py
@@ -1,67 +1,69 @@
 import time
 from datetime import datetime, timedelta
+from typing import List
 
 import pandas as pd
 import pytz
+import typer
 from influxdb_client.client.influxdb_client import InfluxDBClient
 from influxdb_client.client.write.point import Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 from html_scraper import scrape_weather_data
 
+app = typer.Typer()
 
-def format_date(original_date_string):
+
+def format_date(original_date_string: str) -> str:
     original_date = datetime.strptime(original_date_string, '%Y-%m-%d %I:%M %p')
-
-    # Add a timezone (e.g., UTC)
     timezone = pytz.timezone('CET')
     original_date_with_timezone = timezone.localize(original_date)
-
-    # Format the date to include zero-padding and timezone
     return original_date_with_timezone.strftime('%Y-%m-%d %H:%M:%S %Z')
 
 
-stations = ["IARDOO6", "IARDOO10", "IPITTE11"]
-bucket = "wunderground"
-org = "victoor.io"
-token = "ZeJgWpsHbzNwsskV5O2ObJaMo2pnd_OlmXYeM6SK-d1-hxMiPSws3cFA3fOKXDlsuYZPkyswqhxm5w_-NsjcIg=="
-# Store the URL of your InfluxDB instance
-url="http://192.168.1.5:8086"
-
-
-def get_dates(no_of_days=2):
-    # Get the current date
+def get_dates_to_scrape(no_of_days: int) -> List[str]:
     current_date = datetime.now()
     return [(current_date - timedelta(days=i)).strftime('%Y-%m-%d') for i in range(0, no_of_days)]
 
 
-dates = get_dates(1)
-influxdb_client = InfluxDBClient(
-   url=url,
-   token=token,
-   org=org
-)
-write_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
+@app.command()
+def scrape_and_store(
+        days: int = typer.Option(60, help="Number of days to scrape"),
+        bucket: str = typer.Option("wunderground", help="InfluxDB bucket name"),
+        org: str = typer.Option(..., help="InfluxDB organization"),
+        token: str = typer.Option(..., help="InfluxDB access token"),
+        url: str = typer.Option("http://192.168.1.5:8086", help="InfluxDB instance URL"),
+        stations: List[str] = typer.Option(..., help="Weather stations to scrape")
+):
+    dates = get_dates_to_scrape(days)
+    influxdb_client = InfluxDBClient(url=url, token=token, org=org)
+    write_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
 
-for date in dates:
-    for station in stations:
-        url = f"https://www.wunderground.com/dashboard/pws/{station}/table/{date}/{date}/daily"
-        start_time = time.time()
-        df = scrape_weather_data(url)
-        if df is not None:
+    for date in dates:
+        for station in stations:
+            station_url = f"https://www.wunderground.com/dashboard/pws/{station}/table/{date}/{date}/daily"
+            start_time = time.time()
+
+            df = scrape_weather_data(station_url)
+
+            if df is None:
+                continue
+
+            points = []
             for index, row in df.iterrows():
-                timestamp = pd.Timestamp(
-                    format_date(f"{date} {row['Time']}")).value
-                temperature = row['Temperature']
-                humidity = row['Humidity']
-                pressure = row['Pressure']
-                rain = row['Rain']
-                p = (Point(station)
-                     .field("temperature", temperature)
-                     .field("humidity", humidity)
-                     .field("pressure", pressure)
-                     .field("rain", rain)
+                timestamp = pd.Timestamp(format_date(f"{date} {row['Time']}")).value
+                point = (Point(station)
+                     .field("temperature", row['Temperature'])
+                     .field("humidity", row['Humidity'])
+                     .field("pressure", row['Pressure'])
+                     .field("rain", row['Rain'])
                      .time(timestamp))
-                write_api.write(bucket=bucket, org=org, record=p)
-        end_time = time.time()
-        print(f"Finished scraping {url} in {(end_time - start_time):.2f} seconds")
+                points.append(point)
+            write_api.write(bucket=bucket, org=org, record=points)
+            end_time = time.time()
+
+            typer.echo(f"Finished scraping {station_url} in {(end_time - start_time):.2f} seconds")
+
+
+if __name__ == "__main__":
+    app()
